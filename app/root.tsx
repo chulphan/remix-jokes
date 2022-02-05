04be90c44001d58ae4a98b8f6091e40084b84b3b
chulphan <chkim100617@gmail.com>
2022-02-05 23:12:23 +0900

    루트 인덱스에 스타일 추가

--- app/root.tsx
+++ app/root.tsx
@@ -1,25 +1,48 @@
+import type {LinksFunction} from 'remix';
 import {
   LiveReload, Outlet, Links
 } from "remix";
 import type { MetaFunction } from "remix";
+import globalStylesUrl from './styles/global.css';
+import globalLargeStylesUrl from './styles/global-large.css';
+import globalMediumStylesUrl from './styles/global-medium.css';
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return {title: "New Remix App"};
 };
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: globalStylesUrl
+    },
+    {
+      rel: 'stylesheet',
+      href: globalMediumStylesUrl,
+      media: 'print, (min-width: 640px)'
+    },
+    {
+      rel: 'stylesheet',
+      href: globalLargeStylesUrl,
+      media: 'screen and (min-width: 1024px)'
+    }
+  ]
+}
 
 export default function App() {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <title>Remix: So great, it's funny!</title>
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
+    <head>
+      <meta charSet="utf-8"/>
+      <meta name="viewport" content="width=device-width,initial-scale=1"/>
+      <title>Remix: So great, it's funny!</title>
+      <Links/>
+    </head>
+    <body>
+    <Outlet/>
+    {process.env.NODE_ENV === "development" && <LiveReload/>}
+    </body>
     </html>
   );
 }
