bb1f5a2c55de49a467e124fa5d1d6d8ccd324ae3
chulphan <chkim100617@gmail.com>
2022-02-05 11:22:26 +0900

    쓸모없는 코드 및 import 제거

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,5 @@
 import {
-  Links,
   LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
 } from "remix";
 import type { MetaFunction } from "remix";
 
@@ -18,13 +13,10 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
+        <title>Remix: So great, it's funny!</title>
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        Hello world
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
